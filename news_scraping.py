cc330c76d379ac691ebb8693fb030293b12745af
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2022-11-29 19:12:09 +0000

    Added comments to code logic

--- news_scraping.py
+++ news_scraping.py
@@ -10,15 +10,17 @@ import time
 PASS = 'kynzo8-sehFib-dotzaw'
 EMAIL = 't918mgmt@gmail.com'
 
-safari_options = Options()
-
+# Initialize driver, get news site, & maximize window
 driver = webdriver.Safari()
+safari_options = Options()
 driver.get('https://www.reuters.com/business/')
 driver.maximize_window()
 
+# Accept cookie pop-up
 cookie_handler = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
 cookie_handler.click()
 
+# Sign-into website
 sign_in = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
     (By.XPATH, '/html/body/div[1]/div/div[1]/header/div/div/div/div/div[3]/a[1]')))
 sign_in.send_keys(Keys.RETURN)
@@ -31,6 +33,7 @@ email_input = driver.find_element(By.ID, 'email')
 email_input.send_keys(f'{EMAIL}')
 email_input.submit()
 
+# Find Articles on "Business page", loop through each article as WebDriver element and extract first 4 paragraphs
 try:
     PARAGRAPH_LENGTH = 4
     news = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
