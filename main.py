e0728c6580ca4f72adc9c7fcce2043885588bbab
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2022-11-29 13:51:54 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+# Earnings Calendar
+earnings_pm = pd.DataFrame
+earnings_amc = pd.DataFrame
+
+# Ratings
+upgrades = pd.DataFrame
+downgrades = pd.DataFrame
+pt_changes = pd.DataFrame
+initiations = pd.DataFrame
+
+# Futures
+
+
+# Bonds / Forex
+
+
+# Pre-Market Movers
+top_5_active_stocks = pd.DataFrame()
+top_5_gaining_stocks = pd.DataFrame
+top_5_losing_stocks = pd.DataFrame
+
+
+if __name__ == '__main__':
+    pass
