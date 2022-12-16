abcd5314b99b6c38056e85f81c74685cc917bad8
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2022-12-16 21:43:36 +0000

    added pycache, readme, gitignore

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-import numpy as np
-import pandas as pd
-
-# Earnings Calendar
-earnings_pm = pd.DataFrame
-earnings_amc = pd.DataFrame
-
-# Ratings
-upgrades = pd.DataFrame
-downgrades = pd.DataFrame
-pt_changes = pd.DataFrame
-initiations = pd.DataFrame
-
-# Futures
+from scraping.scraper import Scraper
 
 
-# Bonds / Forex
-
-
-# Pre-Market Movers
-top_5_active_stocks = pd.DataFrame()
-top_5_gaining_stocks = pd.DataFrame
-top_5_losing_stocks = pd.DataFrame
-
-
-if __name__ == '__main__':
-    pass
+with Scraper() as scraper:
+    scraper.land_first_page()
