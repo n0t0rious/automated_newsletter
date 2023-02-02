4c857c21684ea570fc1df2b996107867b17c0c8d
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2023-02-02 18:30:21 +0000

    Updated pdf constructor

--- newsletter_gen/pdf_constructor.py
+++ newsletter_gen/pdf_constructor.py
@@ -7,6 +7,7 @@ def generate_newsletter(content: dict, directory: str = None):
     newsletter.add_page()
     newsletter.add_font("DejaVuSans", "", "/Users/gregflorea/PycharmProjects/automated_newsletter/newsletter_gen/DejaVuSans.ttf", uni=True)
     newsletter.set_font('DejaVuSans', size=8)
+    # newsletter.set_font('Arial', size=8)
     for story_num, text in content.items():
         newsletter.multi_cell(
             w=0, h=3, align='L',
