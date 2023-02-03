17bf7c4a9255579f3bbdff6fb693d486844bb4b5
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2023-02-03 14:49:11 +0000

    added requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='automated_newsletter',
+    version='',
+    packages=['scraping', 'newsletter_gen'],
+    url='',
+    license='',
+    author='n0t0rious',
+    author_email='',
+    description=''
+)
